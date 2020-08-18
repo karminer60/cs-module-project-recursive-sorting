ce22c885d09a5551e3a2b16ba9aadd146c8d3494
Karina <GIT SERVICE EMAIL>
2020-08-18 15:53:11 -0700

    initial commit

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,22 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    initial = 0
+    last = len(arr) - 1
+    
 
+    while initial <= last:
+        mid = (initial  + last) // 2
+       
+        if arr[mid] < target:
+            initial = mid + 1
+
+        elif arr[mid] > target:
+            last = mid - 1
+        else:
+            return mid    
+
+    return -1  # not found
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
